ee6f76faa18b5b9ce00cf71584eb129940ef00c2
Charlie <zitongcharliedeng2003@gmail.com>
2023-02-11 18:46:35 +0000

    added accel props to config but idk how they work

--- ApplyMouseConfig.py
+++ ApplyMouseConfig.py
@@ -2,11 +2,29 @@ import math
 import subprocess
 
 # Config
+
 mouseId = 9
-sensitivityMultiplier = 0.1
+
+sensitivityMultiplier = 0.075
+
 degreesOfClockwiseSensorRotation = 11
 
+accelSpeed = 0 # no idea how this works // default = 0
+
+accelSpeedDefault = 0 # no idea how this works // default = 0
+
+accelProfilesAvailable1 = 1 # no idea how this works // default = 1
+accelProfilesAvailable2 = 1 # no idea how this works // default = 1
+
+accelProfileEnabled1 = 0 # no idea how this works // default = 0
+accelProfileEnabled2 = 1 # no idea how this works // default = 1
+
+accelProfileEnabledDefault1 = 1 # no idea how this works // default = 1
+accelProfileEnabledDefault2 = 0 # no idea how this works // default = 0
+
+
 # Logic
+
 r = math.radians(degreesOfClockwiseSensorRotation)
 
     # Rotation Matrix Elements (top-down => left-right) * by scalar sensitivityMultiplier:
@@ -23,6 +41,15 @@ g = 0
 h = 0
 i = 1           
 
+
 # Shell Commands (buncha xinput set-proppings)
-	# Overall Transformation Matrix (i.e. from rotation and sens...)
+
+        # Apply custom accels (don't know what values mean rn) (but default values I put here seem to leave a static "raw" sens with my tests...)
+subprocess.run(["xinput", "--set-prop", f"{mouseId}", "libinput Accel Speed", f"{accelSpeed}"])
+subprocess.run(["xinput", "--set-prop", f"{mouseId}", "libinput Accel Speed Default", f"{accelSpeedDefault}"]) # => BadAccess (attempt to access private resource denied)
+subprocess.run(["xinput", "--set-prop", f"{mouseId}", "libinput Accel Profiles Available", f"{accelProfilesAvailable1}", f"{accelProfilesAvailable2}"])
+subprocess.run(["xinput", "--set-prop", f"{mouseId}", "libinput Accel Profile Enabled", f"{accelProfileEnabled1}", f"{accelProfileEnabled2}"])
+subprocess.run(["xinput", "--set-prop", f"{mouseId}", "libinput Accel Profile Enabled Default", f"{accelProfileEnabledDefault1}", f"{accelProfileEnabledDefault2}"]) # => BadAccess (attempt to access private resource denied)
+
+	# Apply overall Transformation Matrix (i.e. from rotation and sens...)
 subprocess.run(["xinput", "--set-prop", f"{mouseId}", "Coordinate Transformation Matrix", f"{a}",f"{b}",f"{c}",f"{d}",f"{e}",f"{f}",f"{g}",f"{h}",f"{i}"])
